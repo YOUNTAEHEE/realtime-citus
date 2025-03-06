123e7a5e0b6c85adca3775f524361acc5d0a439d
you tae hee <f_hyang@naver.com>
2025-03-06 16:38:28 +0900

    chore: 실시간 페이지 수정2

--- app/realtime/page.jsx
+++ app/realtime/page.jsx
@@ -53,9 +53,9 @@ export default function RealtimePage() {
   const [showAddForm, setShowAddForm] = useState(false);
   const [ws, setWs] = useState(null);
   const [newDevice, setNewDevice] = useState({
-    deviceId: "",
-    name: "",
-    host: "",
+    deviceId: "de3",
+    name: "de3",
+    host: "192.168.10.205",
     port: 502,
     startAddress: 10,
     length: 2,
